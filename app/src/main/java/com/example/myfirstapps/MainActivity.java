97358e0e32099ab1c4521139cf536f81c0a38629
Abdillah AG <abdillahag98@gmail.com>
2025-11-01 15:42:50 +0700

    first commit

---
+++ app/src/main/java/com/example/myfirstapps/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myfirstapps;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ app/src/main/java/com/example/myfirstapps/SecondActivity.java
@@ -0,0 +1,25 @@
+package com.example.myfirstapps;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_second);
+
+        TextView txtWelcome = findViewById(R.id.txtWelcome);
+        String name = "Abdillah AG";
+
+        txtWelcome.setText("Hello, " + name + " Welcome back!");
+    }
+}
